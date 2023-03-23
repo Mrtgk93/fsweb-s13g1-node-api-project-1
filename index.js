e17b3276cc32fa23029c0b3a6800a55db7be27fd
Mrtgk93 <mrtgk93@hotmail.com>
2023-03-23 12:06:42 +0300

    revize heroku için

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const server = require("./api/server");
 
-const port = process.env.Port || 9000;
+const port = process.env.PORT || 9000;
 
 server.listen(port, () => {
   console.log(`Server running at ${port}`);
